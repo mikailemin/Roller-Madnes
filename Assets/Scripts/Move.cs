8f6c8f4aafac33417ce063a08219b14be764b0f7
mikailemin <mikaailaykan@gmail.com>
2023-03-01 15:13:52 +0300

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("mikail");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
