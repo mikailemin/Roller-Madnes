a2e42e95e0b9dc9e2e02298fdc455f63b405550e
mikailemin <mikaailaykan@gmail.com>
2023-03-01 15:48:48 +0300

    +init

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    
+    private Vector3 movement;
+    public float speed;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("mikail");
+       rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        movement = new Vector3(x, 0, z);
+        rb.AddForce(movement * speed * Time.deltaTime);
     }
 }
